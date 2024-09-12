a71f1e415ecf970e395d917b598ff55495b3cf0a
Akhila Wolfe <akhilawolfe@Akhilas-MacBook-Air.local>
2024-09-12 17:33:52 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int[] test_arr1 = new int[10];
+
+    }
+
+    // Step 1.  write a method below that searches through the array and returns the smallest number in the array.
+    //This method must work with any integer array, regardless of size or order
+
+
+    //Step 2.  In the main method, write tests that are robust enough to show that your method works.
+
+
+    //Step 3.  Comment below on how you tested your program
+}
